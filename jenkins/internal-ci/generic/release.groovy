b90f0ba42fb51d47debc279f93f0885cc70a72db
Abhijit Patil <abhijit.patil@seagate.com>
2022-02-16 20:47:43 +0530

    Changes for rpm signin
    Signed-off-by: Abhijit Patil <abhijit.patil@seagate.com>

--- jenkins/internal-ci/generic/release.groovy
+++ jenkins/internal-ci/generic/release.groovy
@@ -311,7 +311,7 @@ pipeline {
                 env.release_build = "${env.release_tag}"
                 env.build_stage = "${build_stage}"
 
-                def toEmail = "abhijit.patil@seagate.com"
+                def toEmail = "shailesh.vaidya@seagate.com"
                 
                 emailext ( 
                         body: '''${SCRIPT, template="K8s-release-email.template"}''',
